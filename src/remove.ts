53c913a69805403c022a8717780a99a09b57cd58
zcs <530627552@qq.com>
2020-05-04 17:41:36 +0800

    fff

--- src/remove.ts
+++ src/remove.ts
@@ -1,20 +1,16 @@
 import path from 'path';
-import { Filter, getFileStat } from './helper';
+import { getFileStat } from './helper';
 import { readdir, unlink, rmdir } from './promise_fs';
 
 export default async function remove(
   target: string,
-  options?: Filter,
+  filter?: (fileName: string, fileType: 'd' | 'f') => Promise<boolean>,
 ): Promise<any> {
   const type = await getFileStat(target);
   if (type === 'n') {
     return;
   }
-  if (
-    options &&
-    options.filter &&
-    (await options.filter(target, type)) === false
-  ) {
+  if (filter && (await filter(target, type)) === false) {
     return;
   }
   if (type === 'f') {
