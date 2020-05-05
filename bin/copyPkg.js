8468cfa9f9becbb7366879c6139166a3240b2f23
zcs <530627552@qq.com>
2020-05-05 20:16:07 +0800

    fix bug

--- bin/copyPkg.js
+++ bin/copyPkg.js
@@ -5,3 +5,7 @@ fs.writeFileSync(
   path.join(__dirname, '../dist/package.json'),
   fs.readFileSync(path.join(__dirname, '../package.json')),
 );
+fs.writeFileSync(
+  path.join(__dirname, '../dist/readme.md'),
+  fs.readFileSync(path.join(__dirname, '../readme.md')),
+);
